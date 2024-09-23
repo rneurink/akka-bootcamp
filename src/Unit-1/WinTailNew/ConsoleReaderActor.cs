6513601bc158f418a5e69b89031d76a7d17d34d2
rneurink <ruben.neurink@gmail.com>
2024-09-23 07:16:35 +0200

    update 1.3

---
+++ src/Unit-1/WinTailNew/ConsoleReaderActor.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Security.Cryptography;
+using Akka.Actor;
+
+namespace WinTailNew
+{
+    /// <summary>
+    /// Actor responsible for reading FROM the console. 
+    /// Also responsible for calling <see cref="ActorSystem.Terminate"/>.
+    /// </summary>
+    internal class ConsoleReaderActor : UntypedActor
+    {
+        public const string StartCommand = "start";
+        public const string ExitCommand = "exit";
+
+        private IActorRef _validationActor;
+
+        public ConsoleReaderActor(IActorRef validationActor)
+        {
+            _validationActor = validationActor;
+        }
+
+        protected override void OnReceive(object message)
+        {
+            if (message.Equals(StartCommand))
+            {
+                DoPrintInstructions();
+            }
+            else if (message is Messages.InputError)
+            {
+                _validationActor.Tell(message as Messages.InputError);
+            }
+
+            GetAndValidateInput();
+        }
+
+        #region Internal methods
+
+        private void DoPrintInstructions()
+        {
+            Console.WriteLine("Write whatever you want into the console!");
+            Console.WriteLine("Some entries will pass validation, and some won't...\n\n");
+            Console.WriteLine("Type 'exit' to quit this application at any time.\n");
+        }
+
+        /// <summary>
+        /// Reads input from console, validates it, then signals appropriate response
+        /// (continue processing, error, success, etc.).
+        /// </summary>
+        private void GetAndValidateInput()
+        {
+            var message = Console.ReadLine();
+            if (!string.IsNullOrEmpty(message) &&
+            String.Equals(message, ExitCommand, StringComparison.OrdinalIgnoreCase))
+            {
+                // if user typed ExitCommand, shut down the entire actor
+                // system (allows the process to exit)
+                Context.System.Terminate();
+                return;
+            }
+
+            // otherwise, just hand message off to validation actor
+            // (by telling its actor ref)
+            _validationActor.Tell(message);
+        }
+
+        #endregion
+    }
+}
---
+++ src/Unit-1/WinTailNew/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using Akka.Actor;
+using Akka.Actor.Setup;
+
+namespace WinTailNew
+{
+    internal class Program
+    {
+        private static ActorSystem MyActorSystem;
+
+        static void Main(string[] args)
+        {
+            MyActorSystem = ActorSystem.Create(nameof(MyActorSystem));
+
+            Props consoleWriterProps = Props.Create<ConsoleWriterActor>();
+            IActorRef consoleWriterActor = MyActorSystem.ActorOf(consoleWriterProps, nameof(consoleWriterActor));
+            
+            Props validationActorProps = Props.Create<ValidationActor>(consoleWriterActor);
+            IActorRef validationActor = MyActorSystem.ActorOf(validationActorProps, nameof(validationActor));
+
+            Props consoleReaderProps = Props.Create<ConsoleReaderActor>(validationActor);
+            IActorRef consoleReaderActor = MyActorSystem.ActorOf(consoleReaderProps, nameof(consoleReaderActor));
+
+            // tell console reader to begin
+            consoleReaderActor.Tell(ConsoleReaderActor.StartCommand);
+
+            // blocks the main thread from exiting until the actor system is shut down
+            MyActorSystem.WhenTerminated.Wait();
+        }
+    }
+}
